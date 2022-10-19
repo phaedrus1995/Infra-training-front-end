9ce560af2844be7f662902eba6f9fffbeabdde7f
Xi Zhang <72356406+phaedrus1995@users.noreply.github.com>
2022-10-19 18:06:33 +0800

    modify the start message

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+  console.log("前端已启动，地址为： http://%s:%s", host, port)
 
 })
